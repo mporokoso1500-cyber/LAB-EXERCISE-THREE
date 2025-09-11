1d1a621b0b055dcd8738b7dbb6c11f3b7cb05733
mporokoso1500-cyber <mporokoso1500@gmail.com>
2025-09-11 09:04:40 +0200

    initial commit

---
+++ LAB EXERCISE TWO.py
@@ -0,0 +1,8 @@
+name = input("what is your name?")
+print ("hello",name+"! nice to meet you,")
+if feleeing.lower 0=="good":
+    print(" thats great to hear! keep smilling,"+ name +")")
+elif feeling.lower 0 =="bad":
+    print ("i`m sorry to hear that,"+ name +", i hope things get better soon,")
+else:
+    print (" thankyou for sharing," "+ name+")
