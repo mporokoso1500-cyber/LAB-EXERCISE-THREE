60fff81c0fa06cf414a20d548807759f0b538f1f
mporokoso1500-cyber <mporokoso1500@gmail.com>
2025-09-28 11:59:34 +0200

    initial commit

---
+++ LAB EXERCISE FOUR.py
@@ -0,0 +1,14 @@
+def print_multiplication_table():
+    num = int(input("Enter a number: "))
+    print(f"Multiplication Table of {num}:")
+    for i in range(1, 13):
+        print(f"{num} x {i} = {num * i}")
+print_multiplication_table()
+
+
+
+
+
+
+
+
